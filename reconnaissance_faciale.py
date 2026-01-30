8e83c6418064e60dab0b8669b256771a6f97348a
TAHIRI Abdelilah <t.abdellilah.t@gmail.com>
2026-01-30 12:58:34 +0100

    Ajout du script de reconnaissance faciale et des assets

---
+++ reconnaissance_faciale.py
@@ -0,0 +1,48 @@
+import threading
+import cv2
+from deepface import DeepFace
+
+# Initialisation du la caméra
+cam = cv2.VideoCapture(0)
+
+# un compteur pour ne pas faire des testes sur chaque frame => une rapidité d'utilisation
+compteur=0
+
+# boolean pour tester si l'image capturer est similaire à l'image réf ou non
+similaire=False
+
+# l'image référence
+imgRef = cv2.imread(r'assets\img1.png')
+
+
+def verifier_visage(frame):
+    global similaire
+    
+    try:
+        if DeepFace.verify(frame,imgRef)['verified']:
+            similaire=True
+        else:
+            similaire = False
+    except ValueError:
+        pass
+
+while True:
+    ret,frame=cam.read()
+
+    if ret:
+        if compteur % 30==0:
+            try:
+                threading.Thread(target=verifier_visage,args=(frame.copy(),)).start()
+            except ValueError:
+                compteur=0
+        compteur+=1
+        if similaire:
+            cv2.putText(frame,'Similaire!',(180,30),cv2.FONT_HERSHEY_SIMPLEX,2,(0,255,0),3)
+        else:
+            cv2.putText(frame,'Non Similaire !!',(180,30),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),3)
+        cv2.imshow('frame',frame)
+    if cv2.waitKey(1)==ord('q'):
+        break
+    
+cv2.destroyAllWindows()
+cam.release()
